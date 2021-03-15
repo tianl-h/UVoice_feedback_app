ab562367abe24dd2a033bd2c63c7372f7e3a068e
tthh1995 <tianlinghou@gmail.com>
2021-03-14 20:20:03 -0400

    update

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 * @Author: tianl
 * @Date:   2021-03-14 16:43:52
 * @Last Modified by:   tianl
-* @Last Modified time: 2021-03-14 20:18:23
+* @Last Modified time: 2021-03-14 20:19:54
 */
 
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({屁屁你好});
+    res.send({hi: 'PiPi'});
 });
 
 // UPPER CASE denot cannot be changed in the future
