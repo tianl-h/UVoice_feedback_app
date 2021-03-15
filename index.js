2b554cf0131b0228fc93be4cfbdd24542f913c9a
tthh1995 <tianlinghou@gmail.com>
2021-03-14 20:16:29 -0400

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 * @Author: tianl
 * @Date:   2021-03-14 16:43:52
 * @Last Modified by:   tianl
-* @Last Modified time: 2021-03-14 17:00:37
+* @Last Modified time: 2021-03-14 20:15:21
 */
 
 const express = require('express');
@@ -12,7 +12,9 @@ app.get('/', (req, res) => {
     res.send({hi: 'there'});
 });
 
-app.listen(5000);
+// UPPER CASE denot cannot be changed in the future
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
 
 
 // localhost:5000
